a9236ee1d204c2d52b27379cfdbfdc3b19338275
lfelipevalerio <l.felipevalerio@hotmail.com>
2019-03-12 19:30:14 -0300

    Update import.sql

--- hellospringboot/src/main/resources/import.sql
+++ hellospringboot/src/main/resources/import.sql
@@ -1,7 +1,7 @@
-insert into temperatura (id, nome, media_notas) values (1, 'Segunda-Feira', 19.6, 24.9, 4.8, '11 Mar - clouds: 76 %,  1015.72 hpa');
-insert into temperatura (id, nome, media_notas) values (2, 'Terça-Feira', 19.7, 22.5, 1.8, '12 Mar - clouds: 48 %,  1014.24 hpa');
-insert into temperatura (id, nome, media_notas) values (3, 'Quarta-Feira', 22.5, 27.6, 7.98, '13 Mar - clouds: 36 %,  1010.75 hpa');
-insert into temperatura (id, nome, media_notas) values (4, 'Qunta-Feira', 19.6, 25.6, 2.29, '14 Mar - clouds: 54 %,  1009.25 hpa');
-insert into temperatura (id, nome, media_notas) values (5, 'Sexta-Feira', 18.4, 22.3, 4.51, '15 Mar - clouds: 86 %,  1014.7 hpa');
-insert into temperatura (id, nome, media_notas) values (6, 'Sábado', 16.8, 20.6, 5.1, '16 Mar - clouds: 85 %,  1017.72 hpa');
-insert into temperatura (id, nome, media_notas) values (7, 'Domingo', 14.2, 22.1, 3.3, '17 Mar - clouds: 20 %,  1015.78 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (1, 'Segunda-Feira', 19.6, 24.9, 4.8, '11 Mar - clouds: 76 %,  1015.72 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (2, 'Terça-Feira', 19.7, 22.5, 1.8, '12 Mar - clouds: 48 %,  1014.24 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (3, 'Quarta-Feira', 22.5, 27.6, 7.98, '13 Mar - clouds: 36 %,  1010.75 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (4, 'Qunta-Feira', 19.6, 25.6, 2.29, '14 Mar - clouds: 54 %,  1009.25 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (5, 'Sexta-Feira', 18.4, 22.3, 4.51, '15 Mar - clouds: 86 %,  1014.7 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (6, 'Sábado', 16.8, 20.6, 5.1, '16 Mar - clouds: 85 %,  1017.72 hpa');
+insert into temperatura (id, diasemana, tempminima, tempmaxima, humidade, descricao) values (7, 'Domingo', 14.2, 22.1, 3.3, '17 Mar - clouds: 20 %,  1015.78 hpa');
